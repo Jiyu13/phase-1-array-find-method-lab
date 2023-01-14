abf9aeb9c3ceb41404e3455bd148df60e1dabf69
Ziru Zou <jiyucoding@yahoo.com>
2023-01-14 16:41:03 -0500

    Use find() to find the win and return the year if win exists, if not return undefined

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(arr) {
+    const found = arr.find(item => item.result === "W")
+    if (found !== undefined) {
+        return found.year;
+    } else {
+        return undefined;
+    }
+}
+
